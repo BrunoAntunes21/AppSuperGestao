c99cf6b8d115362891a3cb2389917ce83c054e1b
bruno antunes <68615491+BrunoAntunes21@users.noreply.github.com>
2022-09-18 23:46:21 -0300

    modificação dos esquemas de cores de algumas views

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -1,1 +1,20 @@
-<h3>pagina de contato em desenvolvimento</h3>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+<body>
+    <style>
+        body{background-color: cadetblue;
+        font-family: 'Times New Roman';
+    font-size: 10px; }
+
+    </style>
+    <h1><i><h3>pagina de contato em desenvolvimento</h3></i></h1>
+
+</body>
+</html>
--- resources/views/site/info.blade.php
+++ resources/views/site/info.blade.php
@@ -1,1 +1,21 @@
-<h3>pagina de informação em desenvolvimento</h3>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+<body>
+    <style>
+        body{background-color: cadetblue;
+        font-family: 'Times New Roman';
+    font-size: 10px; }
+
+    </style>
+    <h1><i>pagina de informação em desenvolvimento</i></h1>
+
+</body>
+</html>
