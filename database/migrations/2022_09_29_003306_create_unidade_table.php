5b9fced775a3bd03d902ead5b90005616283ce8e
bruno antunes <68615491+BrunoAntunes21@users.noreply.github.com>
2022-09-28 23:00:21 -0300

    preciso ver o rollback do create_produto_detalhes_table pois está dando erro

---
+++ database/migrations/2022_09_29_003306_create_unidade_table.php
@@ -0,0 +1,63 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUnidadeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+       /* Schema::table('unidades', function (Blueprint $table) {
+            $table->id();
+            $table->string('unidade',50);
+            $table->string('descricao',30);
+            $table->timestamps();
+            //relacionamnento 1:N
+            //add o relacionamento com a tabela produto
+
+
+        });*/
+         //add o relacionamento com a tabela produto
+        /* Schema::table('produtos',function(Blueprint $table){
+            //$table->unsignedBigInteger('unidade_id');
+            $table->foreign('unidade_id')->references('id')->on('unidades');//[table]_[coluna]_foreing
+         });*/
+            //add o relacionamneto com a tabela produto_detalhe
+
+         Schema::table('produto_detalhes',function(Blueprint $table){
+            $table->unsignedBigInteger('unidade_id');
+            $table->foreign('unidade_id')->references('id')->on('unidades');
+         });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {       //removendo cada uma das integridades relacionais(constraints)
+            //add o relacionamneto com a tabela produto_detalhe
+            Schema::table('produto_detalhes',function(Blueprint $table){
+                //remover a fk
+                $table->dropForeign('produto_detalhes_produto_id_foreign');//[table]_[coluna]_foreing
+                //remover a coluna
+                $table->dropColumn('unidade_id');
+             });
+
+
+             Schema::table('produtos',function(Blueprint $table){
+                //remover a fk
+                $table->dropForeign('produtos_unidade_id_foreign');//[table]_[coluna]_foreing
+                //remover a coluna
+                $table->dropColumn('unidade_id');
+             });
+    }
+}
+
