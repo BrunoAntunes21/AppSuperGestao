ae2e402fcd2101da18e7d40fd66a0fed1c91b385
bruno antunes <68615491+BrunoAntunes21@users.noreply.github.com>
2022-10-07 17:01:56 -0300

    cinfiguração do model para salvar os dados

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\SiteContato;
 use Illuminate\Http\Request;
 
 class ContatoController extends Controller
 {
     public function contatos(Request $request){
 
-        //passando o titulo via array associativo
+        /*passando o titulo via array associativo
         echo '<pre>';
         print_r($request->all());
         echo '<pre>';
@@ -16,6 +17,22 @@ class ContatoController extends Controller
         echo '<br>';
         echo $request->input('email');
         echo '<br>';
+        $contato=new SiteContato();
+        $contato->nome=$request->input('nome');
+        $contato->telefone=$request->input('telefone');
+        $contato->email=$request->input('email');
+        $contato->motivo_contato=$request->input('motivo_contato');
+        $contato->mensagem=$request->input('mensagem');
+
+        print_r($contato->getAttributes());
+
+        //saving the data on the db
+        $contato->save();*/
+        $contato=new SiteContato();
+        $contato->create($request->all());
+        print_r($contato->getAttributes());
+
+
 
         return view('site.contato',['titulo'=>'Contato']);
     }
--- app/SiteContato.php
+++ app/SiteContato.php
@@ -5,6 +5,6 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class SiteContato extends Model
-{
-    //
+{   //configurando o model para poder ussar o metodo fill
+    protected $fillable=['nome','telefone','email','motivo_contato','mensagem'];
 }
